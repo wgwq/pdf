e15d44be6942db274247458f51d3db00774edf08
wgwq <54663743+wgwq@users.noreply.github.com>
2022-01-14 23:26:53 +0300

    Update LibreOffice.php

--- src/Pdf/Docx/Converter/LibreOffice.php
+++ src/Pdf/Docx/Converter/LibreOffice.php
@@ -78,7 +78,7 @@ class LibreOffice extends Container implements DocxConverter
 
 		$this->process = $this->protect(function($cmd)
 		{
-			return new Process(explode(' ', $cmd));
+			return \Symfony\Component\Process\Process::fromShellCommandline($cmd);
 		});
 	}
 
